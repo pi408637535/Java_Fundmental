82ec148744f940ff38535dd358d49fc12867d8da
Galy <piguanghua365@163.com>
2017-03-09 00:31:50 +0800

    type

---
+++ src/main/java/com/it/type/TypeMain.java
@@ -0,0 +1,17 @@
+package com.it.type;
+
+import java.util.ArrayList;
+
+/**
+ * http://developer.51cto.com/art/201103/250028.htm
+ * http://blog.csdn.net/a327369238/article/details/52621043
+ * http://blog.csdn.net/sinat_29581293/article/details/52227953
+ * Created by piguanghua on 2017/3/9.
+ */
+public class TypeMain {
+
+
+    static public void main(String[] args) {
+
+    }
+}
